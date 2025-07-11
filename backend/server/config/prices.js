04b31ad5ee39c6b2aeb9b7e9b39f060f55cc4713
Talha Chaudhary <chaudharytalhaofficial@gmail.com>
2025-07-11 23:42:23 +0500

    update the pricing in stripe

--- backend/server/config/prices.js
+++ backend/server/config/prices.js
@@ -1,6 +1,6 @@
 // config/prices.js (new file for clean separation)
 module.exports = {
-  monthly: 'price_1RbzcuLfWCnW3JgTnYuhvbON',
-  quarterly: 'price_1RbzeCLfWCnW3JgTt1GLdYmH',
-  yearly: 'price_1Rbzf5LfWCnW3JgTTgXL3GyY',
+  monthly: 'price_1Rjlm6LfWCnW3JgTXPBnB3bg',
+  quarterly: 'price_1Rjlo1LfWCnW3JgT1ECbANmU',
+  yearly: 'price_1Rjlp7LfWCnW3JgT2zagpdnt',
 };
